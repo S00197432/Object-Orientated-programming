081ec7a713c22b6f821ec7d900f69faeeefcd150
S00197432 <60605040+S00197432@users.noreply.github.com>
2021-01-11 11:34:26 +0000

    Add files via upload

---
+++ OOP Exam/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exam
+{
+    public abstract class Account
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string InterestDate { get; set; }
+        public float Balance { get; set; }
+        public string AccountType { get; set; }
+
+        public Account(string firstname, string lastname, string interestdate, string accounttype, float balance)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            InterestDate = interestdate;
+            AccountType = accounttype;
+            Balance = balance;
+        }
+        public override string ToString()
+        {
+
+            return string.Format($"{FirstName}" + $"{LastName}" + $"{InterestDate}" + $"{AccountType}" + $"{Balance}");
+        }
+
+    }
+    
+    public class SavingsAccount : Account
+    {
+       
+    }
+    public class CurrentAccount : Account
+    {
+       
+    }
+}
+
---
+++ OOP Exam/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_Exam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Account> AccountList;
+        public List<Account> SavingsAccountList;
+        public List<Account> CurrentAccountList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CHKBXcurrentAccount.IsChecked = true;
+            CHKBXsavingsAccount.IsChecked = true;
+
+            LBXAccountList.ItemsSource = AccountList;
+
+            AccountList = new List<Account>();
+            SavingsAccountList = new List<Account>();
+            CurrentAccountList = new List<Account>();
+            
+            SavingsAccount SA1 = new SavingsAccount() { FirstName = "Adam", LastName = "Hoffman", Balance = 1000, AccountType = "savingsAccount", InterestDate = "11/01/2021" };
+            CurrentAccount CA1 = new CurrentAccount() { FirstName = "Iron", LastName = "Man", Balance = 100000000, AccountType = "Current Account", InterestDate = "11/01/2021" };
+
+            AccountList.Add(SA1);
+            AccountList.Add(CA1);
+
+            SavingsAccountList.Add(SA1);       
+            CurrentAccountList.Add(CA1);
+
+            Account account;
+        }
+
+        private void CHKBXsavingsAccount_Checked(object sender, RoutedEventArgs e)
+        {
+            if (CHKBXcurrentAccount.IsChecked == false && CHKBXsavingsAccount.IsChecked == true)
+            {
+                LBXAccountList.ItemsSource = SavingsAccountList;
+
+            }
+        }
+
+        private void CHKBXcurrentAccount_Checked(object sender, RoutedEventArgs e)
+        {
+            if (CHKBXcurrentAccount.IsChecked == true && CHKBXsavingsAccount.IsChecked == false)
+            {
+                LBXAccountList.ItemsSource = CurrentAccountList;
+            }
+        }
+
+        private void BTNDeposit_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
